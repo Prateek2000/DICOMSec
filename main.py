6a8f1cc0e3878282120552f566992d9e33562a40
Prateek2000 <sharma.pratik2000@gmail.com>
2022-03-28 16:28:01 +0530

    Rename deidentification.py to anonymization.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from PIL import Image
 import matplotlib.pyplot as plt
 from matplotlib import cm
 import numpy as np
-from deidentification import anonymise
+from anonymization import anonymise
 
 if __name__ == "__main__":
     ds = pydicom.dcmread("sample.dcm")
